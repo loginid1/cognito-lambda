1c159c1d858f9b4f9b777045b7af1e0600ad6e0a
braagaa <michael_braga@outlook.com>
2023-08-15 23:36:43 -0400

    deleted service files

--- client/services/user.ts
+++
@@ -1,9 +0,0 @@
-import { get } from "./fetch";
-import { User } from "../contexts/types";
-
-const BASE_URL = "/api/user";
-
-export const checkAuth = async () => {
-  const response = get<User>(BASE_URL);
-  return response;
-};
