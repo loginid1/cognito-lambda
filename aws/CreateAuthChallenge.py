0e0da1870c8a9b16451a810fed1509ae43b26271
braagaa <michael_braga@outlook.com>
2023-11-21 16:38:16 -0500

    updated lambdas to new flow

--- aws/CreateAuthChallenge.py
+++ aws/CreateAuthChallenge.py
@@ -4,6 +4,7 @@ import re
 import requests
 
 from loginid import LoginID
+from loginid.utils import LoginIDError
 from os import environ
 
 LOGINID_BASE_URL = environ.get("LOGINID_BASE_URL") or ""
@@ -16,18 +17,55 @@ def lambda_handler(event: dict, _: dict) -> dict:
     print(event)
     lid = LoginID(LOGINID_BASE_URL, get_private_key())
 
+    request = event["request"]
     response = event["response"]
     username = event["userName"]
+    session = request.get("session")
+    client_metadata = request.get("clientMetadata")
 
-    try:
+    # if session does not exist or if session length is 0 then create dummy challenge
+    # to obtain authentication type
+    if not session or len(session) == 0:
+        response["challengeMetadata"] = "AUTH_PARAMS"
+        response["privateChallengeParameters"] = {
+            "challenge": "AUTH_PARAMS"
+        }
+        response["publicChallengeParameters"] = {
+            "challenge": "AUTH_PARAMS"
+        }
+
+        return event
+
+    if not client_metadata:
+        raise Exception("ClientMetadata is required")
+
+    init_res = None
+    public_key = None
+
+    # metadata will contain authentication type
+
+    # sign in with FIDO2
+    if client_metadata["authentication_type"] == "FIDO2_GET":
         init_res = lid.authenticate_fido2_init(username)
-    except Exception as e:
-        print(e)
-        raise e
+        public_key = json.dumps(init_res)
 
-    public_key = json.dumps(init_res)
+    # add FIDO2 credential to existing user
+    elif client_metadata["authentication_type"] == "FIDO2_CREATE":
+        try:
+            init_res = lid.add_fido2_credential_init(username)
+            public_key = json.dumps(init_res)
+        except LoginIDError as e:
+            if e.status == 404 and e.code == "unknown_user":
+                lid.create_user_without_credential(username)
+                init_res = lid.add_fido2_credential_init(username)
+                public_key = json.dumps(init_res)
+            else:
+                print(e)
+                raise e
 
-    # can probably remove this
+    else:
+        raise Exception("Authentication type not supported")
+
     response["privateChallengeParameters"] = {
         "public_key": public_key
     }
--- aws/DefineAuthChallenge.py
+++ aws/DefineAuthChallenge.py
@@ -3,7 +3,7 @@ def lambda_handler(event: dict, _: dict) -> dict:
 
     request, response = event["request"], event["response"]
 
-    # start FIDO2 challenge
+    # check if user is not found
     if request.get("userNotFound"):
         response["issueToken"] = False
         response["failAuthentication"] = True
@@ -11,21 +11,43 @@ def lambda_handler(event: dict, _: dict) -> dict:
 
     session = request.get("session", [])
 
+    # custom challenge
     if not len(session):
         response["issueTokens"] = False
         response["failAuthentication"] = False
         response["challengeName"] = "CUSTOM_CHALLENGE"
 
+    # if sessions exist
     if len(session):
+        # metadata will be used to determine the type of authentication
+        client_metadata = request.get("clientMetadata", {})
+        if not client_metadata:
+            raise Exception("Client metadata not found")
+
+        authentication_type = client_metadata.get("authentication_type")
         session_obj = session[-1]
+        challenge_metadata = session_obj.get("challengeMetadata")
 
         challenge_name = session_obj["challengeName"]
         challenge_result = session_obj["challengeResult"]
 
-        # check if FIDO2 challenge succeeded
-        if challenge_name == "CUSTOM_CHALLENGE" and challenge_result:
-            response["issueTokens"] = True
+        # will be used as the first round of authentication to get provided authentication type
+        if challenge_metadata == "AUTH_PARAMS":
+            # renew challenge
+            response["issueTokens"] = False
             response["failAuthentication"] = False
+            response["challengeName"] = "CUSTOM_CHALLENGE"
+
+        valid_types = ["FIDO2_CREATE", "FIDO2_GET", "FIDO2_ADD"]
+
+        if valid_types.count(authentication_type):
+            challenge_name = session_obj["challengeName"]
+            challenge_result = session_obj["challengeResult"]
+
+            # check if FIDO2 challenge succeeded
+            if challenge_name == "CUSTOM_CHALLENGE" and challenge_result:
+                response["issueTokens"] = True
+                response["failAuthentication"] = False
 
         return event
 
